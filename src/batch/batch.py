e017a35b03717a0f6b18e92be1e3887c112eff05
Riju-bak <defaltsith@gmail.com>
2022-03-22 17:04:13 -0400

    minor refactor

--- src/batch/batch.py
+++ src/batch/batch.py
@@ -1,40 +1,54 @@
 from scipy.integrate import odeint
 import numpy as np
-np.seterr(divide='ignore', invalid='ignore')
 import matplotlib.pyplot as plt
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
+np.seterr(divide='ignore', invalid='ignore')
+
+load_dotenv()
+
+# Kinetic Params
+KiC = float(os.environ.get("KiC"))
+KiN = float(os.environ.get("KiN"))
+KiX = float(os.environ.get("KiX"))
+KiS = float(os.environ.get("KiS"))
+KN = float(os.environ.get("KN"))
+KO1 = float(os.environ.get("KO1"))
+KO2 = float(os.environ.get("KO2"))
+KS = float(os.environ.get("KS"))
+KSE = float(os.environ.get("KSE"))
+KSL = float(os.environ.get("KSL"))
+mS = float(os.environ.get("mS"))
+rE = float(os.environ.get("rE"))
+rL = float(os.environ.get("rL"))
+YCS = float(os.environ.get("YCS"))
+YLS = float(os.environ.get("YLS"))
+YXN = float(os.environ.get("YXN"))
+YXS = float(os.environ.get("YXS"))
+aE = float(os.environ.get("aE"))
+aL = float(os.environ.get("aL"))
+bCmax = float(os.environ.get("bCmax"))
+mu_max = float(os.environ.get("mu_max"))
+
+# Init vals
+SF = float(os.environ.get("SF"))
+O0 = float(os.environ.get("O0"))
+O = O0  # O is assumed constant
+
+S0 = float(os.environ.get("S0"))
+N0 = float(os.environ.get("N0"))
+X0 = float(os.environ.get("X0"))
+V0 = float(os.environ.get("V0"))
+L0 = float(os.environ.get("L0"))
+E0 = float(os.environ.get("E0"))
+LE0 = float(os.environ.get("LE0"))
+C0 = float(os.environ.get("C0"))
+
+
+# OUR0 = float(os.environ.get("OUR0"))
 
 def odes(y, t):
-    # Kinetic Params
-    KiC = float(os.environ.get("KiC"))
-    KiN = float(os.environ.get("KiN"))
-    KiX = float(os.environ.get("KiX"))
-    KiS = float(os.environ.get("KiS"))
-    KN = float(os.environ.get("KN"))
-    KO1 = float(os.environ.get("KO1"))
-    KO2 = float(os.environ.get("KO2"))
-    KS = float(os.environ.get("KS"))
-    KSE = float(os.environ.get("KSE"))
-    KSL = float(os.environ.get("KSL"))
-    mS = float(os.environ.get("mS"))
-    rE = float(os.environ.get("rE"))
-    rL = float(os.environ.get("rL"))
-    YCS = float(os.environ.get("YCS"))
-    YLS = float(os.environ.get("YLS"))
-    YXN = float(os.environ.get("YXN"))
-    YXS = float(os.environ.get("YXS"))
-    aE = float(os.environ.get("aE"))
-    aL = float(os.environ.get("aL"))
-    bCmax = float(os.environ.get("bCmax"))
-    mu_max = float(os.environ.get("mu_max"))
-
-    # Init vals
-    SF = float(os.environ.get("SF"))
-    O0 = float(os.environ.get("O0"))
-    O = O0  # O is assumed constant
-
     # assign each ODE to a vector elem
     Xf = y[0]
     C = y[1]
@@ -46,63 +60,53 @@ def odes(y, t):
     V = y[7]
 
     # ########## ODE Definitions #####################
-    FB = V / 1000 * (7.14 / YXN * (mu_max * (N / (KN + N)) * (O / (KO1 + O)) * (S / (KS + S)) * (KiS / (KiS + S)) * (KiX / (KiX + Xf))) * Xf + 1.59 * ((1 - rL) * (bCmax * (KiN / (KiN + N)) * (O / (KO2 + O)) * (S / (KS + S)) * (KiS / (KiS + S)) * (KiX / (KiX + Xf)) * ((KiC - C / Xf) / KiC))) * Xf)
+    mu = (mu_max * (N / (KN + N)) * (O / (KO1 + O)) * (S / (KS + S)) * (KiS / (KiS + S)) * (KiX / (KiX + Xf)))
+    bLC = (bCmax * (KiN / (KiN + N)) * (O / (KO2 + O)) * (S / (KS + S)) * (KiS / (KiS + S)) * (KiX / (KiX + Xf)) * (
+                (KiC - C / Xf) / KiC))
+    bC = ((1 - rL) * bLC)
+    FB = V / 1000 * (7.14 / YXN * mu * Xf + 1.59 * bC * Xf)
 
-    bL = rL * (bCmax * (KiN / (KiN + N)) * (O / (KO2 + O)) * (S / (KS + S)) * (KiS / (KiS + S)) * (KiX / (KiX + Xf)) * ((KiC - C / Xf) / KiC)) - KSL * (L / (Xf + L)) * (O / (KO2 + O))
-    qS = (mu_max * (N / (KN + N)) * (O / (KO1 + O)) * (S / (KS + S)) * (KiS / (KiS + S)) * (KiX / (KiX + Xf))) / YXS + (O / (KO1 * Xf + O)) * (S / (KS + S)) * mS + ((1 - rL) * (bCmax * (KiN / (KiN + N)) * (O / (KO2 + O)) * (S / (KS + S)) * (KiS / (KiS + S)) * (KiX / (KiX + Xf)) * ((KiC - C / Xf) / KiC))) / YCS + (aL * (mu_max * (N / (KN + N)) * (O / (KO1 + O)) * (S / (KS + S)) * (KiS / (KiS + S)) * (KiX / (KiX + Xf))) + bL) / YLS
+    bL = rL * bLC - KSL * (L / (Xf + L)) * (O / (KO2 + O))
+    qS = mu / YXS + (O / (KO1 * Xf + O)) * (S / (KS + S)) * mS + bC / YCS + (aL * mu + bL) / YLS
 
     FS = 0 if S>20 else (qS * Xf + FB * S / V) * (V / (SF - S))  # Glucose Feed Rate (L/h)
     D = (FB + FS) / V
 
-    dXfdt = (mu_max * (N / (KN + N)) * (O / (KO1 + O)) * (S / (KS + S)) * (KiS / (KiS + S)) * (KiX / (KiX + Xf))) * Xf - D * Xf
+    dXfdt = mu * Xf - D * Xf
 
-    dCdt = (1.88 * (1 - rL) * (bCmax * (KiN / (KiN + N)) * (O / (KO2 + O)) * (S / (KS + S)) * (KiS / (KiS + S)) * (KiX / (KiX + Xf)) * ((KiC - C / Xf) / KiC))) * Xf - D * C
+    qC = (1.88 * (1 - rL) * bLC)
+    dCdt = qC * Xf - D * C
 
-    qL = aL * (mu_max * (N / (KN + N)) * (O / (KO1 + O)) * (S / (KS + S)) * (KiS / (KiS + S)) * (KiX / (KiX + Xf))) + bL
+    qL = aL * mu + bL
     dLdt = qL * Xf - D * L
-    bE = rE * (bCmax * (KiN / (KiN + N)) * (O / (KO2 + O)) * (S / (KS + S)) * (KiS / (KiS + S)) * (KiX / (KiX + Xf)) * ((KiC - C / Xf) / KiC)) * (O / (KO2 + O)) - KSE * (E / (Xf + L)) * (O / (KO2 + O))
-    dLEdt = ((aL - aE) * (mu_max * (N / (KN + N)) * (O / (KO1 + O)) * (S / (KS + S)) * (KiS / (KiS + S)) * (KiX / (KiX + Xf))) + (bL - bE)) * Xf - D * LE
+    bE = rE * bLC * (O / (KO2 + O)) - KSE * (E / (Xf + L)) * (O / (KO2 + O))
+    dLEdt = ((aL - aE) * mu + (bL - bE)) * Xf - D * LE
 
-    qE = aE * (mu_max * (N / (KN + N)) * (O / (KO1 + O)) * (S / (KS + S)) * (KiS / (KiS + S)) * (KiX / (KiX + Xf))) + bE
+    qE = aE * mu + bE
     dEdt = qE * Xf - D * E
 
     dSdt = (-qS * Xf - (FB * S/V)) if S>20 else 0
 
-    qN = (mu_max * (N / (KN + N)) * (O / (KO1 + O)) * (S / (KS + S)) * (KiS / (KiS + S)) * (KiX / (KiX + Xf))) / YXN
+    qN = mu / YXN
     dNdt = -qN * Xf - D * N
 
     dVdt = FS + FB
 
-    # TODO: Fix OUR
-    mu = (mu_max * (N / (KN + N)) * (O / (KO1 + O)) * (S / (KS + S)) * (KiS / (KiS + S)) * (KiX / (KiX + Xf)))
-    Bc = ((1 - rL) * (bCmax * (KiN / (KiN + N)) * (O / (KO2 + O)) * (S / (KS + S)) * (KiS / (KiS + S)) * (
-                KiX / (KiX + Xf)) * ((KiC - C / Xf) / KiC)))
-    qO2 = -(1.07 * qS - 1.37 * mu - 2.86 * (aL * mu + bL) - 1.45 * Bc)
-    OUR = 31.25 * qO2 * Xf
-###############################################################################################################################################
+    ###############################################################################################################################################
 
-    return [dXfdt, dCdt, dLdt, dLEdt, dEdt, dSdt, dNdt, dVdt, OUR]
+    return [dXfdt, dCdt, dLdt, dLEdt, dEdt, dSdt, dNdt, dVdt]
 
-load_dotenv()
-
-S0 = float(os.environ.get("S0"))
-N0 = float(os.environ.get("N0"))
-X0 = float(os.environ.get("X0"))
-V0 = float(os.environ.get("V0"))
-L0 = float(os.environ.get("L0"))
-E0 = float(os.environ.get("E0"))
-LE0 = float(os.environ.get("LE0"))
-C0 = float(os.environ.get("C0"))
-OUR0 = float(os.environ.get("OUR0"))
 
 # Initial Conditions
-y0 = [X0, C0, L0, LE0, E0, S0, N0, V0, OUR0]
+y0 = [X0, C0, L0, LE0, E0, S0, N0, V0]
 
 # Test ODEs
 # print(odes(y=y0, t=0))
 
 # declare a time vector(time window)
-t = np.linspace(0, 144, 1000)
+T = 144  # Total batch operation time(h)
+T_split = 10000
+t = np.linspace(0, T, T_split)
 y = odeint(odes, y0, t)
 
 Xf = y[:, 0]
@@ -113,7 +117,7 @@ E = y[:, 4]
 S = y[:, 5]
 N = y[:, 6]
 V = y[:, 7]
-OUR = y[:, 8]
+# OUR = y[:, 8]
 
 # EPA_titer = E/X*100
 EFT = 120 # Effective fermentation time(h)
@@ -127,7 +131,21 @@ EPA_percent_Biomass = E * 100 / X
 Lipid_percent_Biomass = L*100/X
 EPA_percent_Lipid = E*100/L
 
-
+# OUR calculation
+# mu = (mu_max * (N / (KN + N)) * (O / (KO1 + O)) * (S / (KS + S)) * (KiS / (KiS + S)) * (KiX / (KiX + Xf)))
+# Bc = ((1 - rL) * (bCmax * (KiN / (KiN + N)) * (O / (KO2 + O)) * (S / (KS + S)) * (KiS / (KiS + S)) * (
+#         KiX / (KiX + Xf)) * ((KiC - C / Xf) / KiC)))
+# bL = rL * (bCmax * (KiN / (KiN + N)) * (O / (KO2 + O)) * (S / (KS + S)) * (KiS / (KiS + S)) * (KiX / (KiX + Xf)) * (
+#             (KiC - C / Xf) / KiC)) - KSL * (L / (Xf + L)) * (O / (KO2 + O))
+# qS = (mu_max * (N / (KN + N)) * (O / (KO1 + O)) * (S / (KS + S)) * (KiS / (KiS + S)) * (KiX / (KiX + Xf))) / YXS + (
+#             O / (KO1 * Xf + O)) * (S / (KS + S)) * mS + ((1 - rL) * (
+#             bCmax * (KiN / (KiN + N)) * (O / (KO2 + O)) * (S / (KS + S)) * (KiS / (KiS + S)) * (KiX / (KiX + Xf)) * (
+#                 (KiC - C / Xf) / KiC))) / YCS + (aL * (
+#             mu_max * (N / (KN + N)) * (O / (KO1 + O)) * (S / (KS + S)) * (KiS / (KiS + S)) * (
+#                 KiX / (KiX + Xf))) + bL) / YLS
+# qO2 = -(1.07 * qS - 1.37 * mu - 2.86 * (aL * mu + bL) - 1.45 * Bc)
+# OUR = 31.25 * qO2 * Xf
+#####################################################
 
 #Plotting X
 plt.figure()
@@ -145,11 +163,11 @@ plt.plot(t, Lipid_percent_Biomass)
 plt.draw()
 
 #Plotting OUR
-plt.figure()
-plt.title("OUR(mmol/L/h)")
-plt.xlabel("time(h)")
-plt.plot(t, OUR)
-plt.draw()
+# plt.figure()
+# plt.title("OUR(mmol/L/h)")
+# plt.xlabel("time(h)")
+# plt.plot(t, OUR)
+# plt.draw()
 
 #Plotting EPA(% Liquid)
 plt.figure()
@@ -188,6 +206,13 @@ plt.xlabel("t")
 plt.plot(t, N)
 plt.draw()
 
+# Plotting E
+plt.figure()
+plt.title("E vs t")
+plt.ylabel("E")
+plt.xlabel("t")
+plt.plot(t, E)
+plt.draw()
+
+
 plt.show()
-
-
