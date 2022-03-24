250d0b8cbbb66afd1c55514ae1ed5634b3afed2e
Xuan Nguyen <nminhxuan@gmail.com>
2022-03-23 21:17:16 -0400

    continuous modified

--- cont.py
+++ cont.py
@@ -21,86 +21,82 @@ aL = 0.017
 bCmax = 0.090
 mmax = 0.26
 
-# Number of Stage
-#n = input("Number of stage: ")
-#n = int(n)
-
-# Operating Conditions Input
-V = 1.5
-O = 25
-
-SF = float(input("Glucose Concentration in Feed 1: "))
-CN = float(input("Ratio C/N in Feed 1: "))
-D = float(input("Diluation rate of stage 1: "))
-
-NF = (SF*6*12/180) / CN
-
-#Solve Non-linear Equations - Stage 1
 import numpy as np
 from scipy import stats
 from scipy import optimize
-def equations(vars):
-    FB, S, N, Xf, L, E, C  = vars
-    m = mmax*(N/(KN+N))*(O/(KO1+O))*(S/(KS+S))*(KiS/(KiS+S))*(KiX/(KiX+Xf))
-    bLC = bCmax*(KiN/(KiN+N))*(O/(KO2+O))*(S/(KS+S))*(KiS/(KiS+S))*(KiX/(KiX+Xf))*((KiC - (C/Xf))/KiC)
-    bC = (1-rL)*bLC
-    bL = rL*bLC - KSL*(L/(Xf+L))*(O/(KO2+O))
-    bE = rE*bLC*(O/(KO2+O)) - KSE*(E/(Xf+L))*(O/(KO2+O))
-    qS = m/YXS + (O/(KO1*Xf + O))*(S/(KS + S))*mS + bC/YCS + (aL*m + bL)/YLS
-    qN = m/YXN
-    qL = aL*m + bL
-    qE = aE*m + bE
-    qC = 1.88*(1-rL)*bLC
-    eq1 = FB - ((V/1000) * ((7.14/YXN)*m*Xf + 1.59*bC*Xf))
-    FS = D*V - FB
-    F = D*V
-    eq2 = -F*Xf + m*Xf*V
-    eq3 = FS*SF - F*S - qS*Xf*V
-    eq4 = FS*NF - F*N - qN*Xf*V
-    eq5 = -F*L + qL*Xf*V
-    eq6 = -F*E + qE*Xf*V
-    eq7 = -F*C + qC*Xf*V
-    return (eq1, eq2, eq3, eq4, eq5, eq6, eq7)
-def f(vars):
-    return abs(np.array(sum(equations(vars))**2)-0)
-FB, S, N, Xf, L, E, C =  optimize.fsolve(equations, (optimize.fmin(f, (0.02, 0.095, 0.01, 150, 100, 56, 100), xtol=0.01, maxiter=100)))
-print("FB = ", FB*1000, " FS = ", (D*V-FB)*1000," S = ", S, " N = ", N, " Xf = ", Xf," L = ", L, " E = ", E, " C = ", C, " t = ", 1/D )
+from pickle import TRUE
+import pandas as pd
 
-#n-stage
+# Operating Conditions Input
+n = int(input("Number of stage: "))
+O = 25
 
+# n-stage
+data = []
+for a in range (0, n+1):
+    if a == 0:
+        SF = 0
+        D = 0
+        V = 0
+        S = 0
+        N = 0
+        Xf = 0
+        L = 0
+        E = 0
+        C = 0 
+        F = 0
+    else:
+        print("Inputs for stage {}".format(a))
+        SF = float(input("Glucose Concentration in Feed {} (g/L): ".format(a)))
+        D = float(input("Dilution Rate of Stage {}: ".format(a)))
+        if a == 1:
+            V = float(input("Volume of stage-1 reactor (L): "))
+            CN = float(input("Ratio C/N in Feed 1: "))
+            NF = (SF*6*12/180) / CN
+        else:
+            VRatio = float(input("Volume Ratio V{}/V{}: ".format(a, a-1)))
+            V = V*VRatio
+            F = V*D
+    name = ['SF', 'D', 'V', 'S', 'N', 'Xf', 'L', 'E', 'C']
+    value = [SF, D, V, S, N, Xf, L, E, C]
+    data.append((dict(zip(name, value))))
+df = pd.DataFrame(data)
 
-#Validate results 
-m = mmax*(N/(KN+N))*(O/(KO1+O))*(S/(KS+S))*(KiS/(KiS+S))*(KiX/(KiX+Xf))
-bLC = bCmax*(KiN/(KiN+N))*(O/(KO2+O))*(S/(KS+S))*(KiS/(KiS+S))*(KiX/(KiX+Xf))*((KiC - (C/Xf))/KiC)
-bC = (1-rL)*bLC
-bL = rL*bLC - KSL*(L/(Xf+L))*(O/(KO2+O))
-bE = rE*bLC*(O/(KO2+O)) - KSE*(E/(Xf+L))*(O/(KO2+O))
-qS = m/YXS + (O/(KO1*Xf + O))*(S/(KS + S))*mS + bC/YCS + (aL*m + bL)/YLS
-qN = m/YXN
-qL = aL*m + bL
-qE = aE*m + bE
-qC = 1.88*(1-rL)*bLC
-FS = D*V - FB
-F = D*V
-
-
-print(-F*Xf + m*Xf*V)
-print(FS*SF - F*S - qS*Xf*V)
-print(FS*NF - F*N - qN*Xf*V)
-print(-F*L + qL*Xf*V)
-print(-F*E + qE*Xf*V)
-print(-F*C + qC*Xf*V)
-
-EFT = V/F # Effective fermentation time(h)
-EPA_rate = E/EFT
+     #Solve Non-linear Equations
+for a in range (1, n+1, 1):     
+    def equations(vars):
+        FB, S, N, Xf, L, E, C  = vars
+        m = mmax*(N/(KN+N))*(O/(KO1+O))*(S/(KS+S))*(KiS/(KiS+S))*(KiX/(KiX+Xf))
+        bLC = bCmax*(KiN/(KiN+N))*(O/(KO2+O))*(S/(KS+S))*(KiS/(KiS+S))*(KiX/(KiX+Xf))*((KiC - (C/Xf))/KiC)
+        bC = (1-rL)*bLC
+        bL = rL*bLC - KSL*(L/(Xf+L))*(O/(KO2+O))
+        bE = rE*bLC*(O/(KO2+O)) - KSE*(E/(Xf+L))*(O/(KO2+O))
+        qS = m/YXS + (O/(KO1*Xf + O))*(S/(KS + S))*mS + bC/YCS + (aL*m + bL)/YLS
+        qN = m/YXN
+        qL = aL*m + bL
+        qE = aE*m + bE
+        qC = 1.88*(1-rL)*bLC
+        FS = df.loc[(a), 'D']*df.loc[(a), 'V'] - FB
+        F = df.loc[(a), 'D']*df.loc[(a), 'V']
+        eq1 = FB - ((df.loc[(a), 'V']/1000) * ((7.14/YXN)*m*Xf + 1.59*bC*Xf))
+        eq2 = F*df.loc[(a-1), 'Xf'] - F*Xf + m*Xf*df.loc[(a), 'V']
+        eq3 = F*df.loc[(a-1), 'S'] + FS*df.loc[(a), 'SF'] - F*S - qS*Xf*df.loc[(a), 'V']
+        eq4 = F*df.loc[(a-1), 'N'] + FS*NF - F*N - qN*Xf*df.loc[(a), 'V']
+        eq5 = F*df.loc[(a-1), 'L'] - F*L + qL*Xf*df.loc[(a), 'V']
+        eq6 = F*df.loc[(a-1), 'E'] - F*E + qE*Xf*df.loc[(a), 'V']
+        eq7 = F*df.loc[(a-1), 'C'] - F*C + qC*Xf*df.loc[(a), 'V']
+        return (eq1, eq2, eq3, eq4, eq5, eq6, eq7)
+    def f(vars):
+        return abs(np.array(sum(equations(vars))**2)-0)
+    FB, S, N, Xf, L, E, C =  optimize.fsolve(equations, (optimize.fmin(f, (0.02, 0.095, 0.01, 150, 100, 56, 100), xtol=0.01, maxiter=1000)))
+    df.loc[a, 'S'], df.loc[a, 'N'], df.loc[a, 'Xf'], df.loc[a, 'L'], df.loc[a, 'E'], df.loc[a, 'C'] = S, N, Xf, L, E, C
+df = np.round(df, decimals = 2)
+print(df)
 
 #Evaluation Results 
-X = Xf + L
-EPA_titer = E * 100 / X
-Lipid_percent_Biomass = L*100/X
-EPA_percent_Lipid = E*100/L
+t = (df['V'].sum()) / (D*V)
+EPA_titer = E*100/(Xf+L)
+EPA_rate = E/t
 
-print("EPA titer: ", EPA_titer)
-print("Lipid_percent_Biomass: ", Lipid_percent_Biomass)
-print("EPA_percent_Lipid: ", EPA_percent_Lipid)
-print("EPA_rate: ", EPA_rate)
+print("EPA titer: ", round(EPA_titer))
+print("EPA_rate: ", round(EPA_rate))
